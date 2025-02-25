a80d9c554db7d781a3d34e2998d4d552f4c706ed
Parth Gandhi <parthgandhi@outlook.com>
2025-02-25 20:10:15 +0530

    run example

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from geoguessr-agent!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from browser_use import Agent
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o")
+
+
+async def main():
+    agent = Agent(
+        task="Compare the price of gpt-4o and DeepSeek-V3",
+        llm=llm,
+    )
+    result = await agent.run()
+    print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
